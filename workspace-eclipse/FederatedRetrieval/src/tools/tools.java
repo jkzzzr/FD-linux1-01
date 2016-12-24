6f2108a7ad0058b9f093568b66e0086a94059d17
Lee <Lee@localhost.centos6.5>
2016-12-24 17:39:05 +0800

    读取硬盘上的09年的数据，然后对读取这些.war.gz文件，输出其中包含的docid和url和这个doc在.war.gz文件中的输出流节点

---
+++ workspace-eclipse/FederatedRetrieval/src/tools/tools.java
@@ -0,0 +1,12 @@
+package tools;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class tools {
+	public static void run(String temp) throws Exception{
+		BufferedWriter bWriter = new BufferedWriter(new FileWriter("/home/Lee/data/err",true));
+		bWriter.write(temp+"\n");
+		bWriter.close();
+	}
+}
